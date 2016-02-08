0fb79e2c6ac0518f1fb789079821927a60c0a1ed
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2016-02-08 21:39:26 +0000

    Version 2: read all files and search at once (having a problem).

--- src/elasticsearch/Checker.java
+++ src/elasticsearch/Checker.java
@@ -1,5 +1,6 @@
 package elasticsearch;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.nio.charset.Charset;
@@ -14,16 +15,21 @@ public class Checker {
 	public static void main(String[] args) {
 		try {
 			es.startup();
-			// read the content of the file
-			String content = readFile(args[0], Charset.defaultCharset());
-			JavaTokenizer tokenizer = new JavaTokenizer(Settings.Normalize.MED_NORM);
-			ArrayList<String> tokens = tokenizer.getTokensFromFile(args[0]);
-			String query = printArray(tokens, false);
-			Path p = Paths.get(args[0]);
-			String file = p.getFileName().toString();
-			System.out.print(file + ",");
-			// System.out.print(file.split("\\$")[0] + ",");
-			System.out.println(findTP(es.search(query), file.split("\\$")[0]));
+			File folder = new File(args[0]);
+			File[] listOfFiles = folder.listFiles();
+
+			for (int i = 0; i < listOfFiles.length; i++) {
+				// read the content of the file
+				String content = readFile(listOfFiles[i].getAbsolutePath(), Charset.defaultCharset());
+				JavaTokenizer tokenizer = new JavaTokenizer(Settings.Normalize.MED_NORM);
+				ArrayList<String> tokens = tokenizer.getTokensFromFile(listOfFiles[i].getAbsolutePath());
+				String query = printArray(tokens, false);
+				//Path p = Paths.get(listOfFiles[i].getName());
+				// String file = p.getFileName().toString();
+				System.out.print(listOfFiles[i].getName() + ",");
+				// System.out.print(file.split("\\$")[0] + ",");
+				System.out.println(findTP(es.search(query), listOfFiles[i].getName().split("\\$")[0]));
+			}
 			es.shutdown();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -33,10 +39,10 @@ public class Checker {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static int findTP(ArrayList<String> results, String query) {
 		int tp = 0;
-		for (int i=0; i<results.size(); i++) {
+		for (int i = 0; i < results.size(); i++) {
 			// System.out.println(results.get(i));
 			if (results.get(i).contains(query)) {
 				tp++;
@@ -49,7 +55,7 @@ public class Checker {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded, encoding);
 	}
-	
+
 	public static String printArray(ArrayList<String> arr, boolean pretty) {
 		String s = "";
 		for (int i = 0; i < arr.size(); i++) {
