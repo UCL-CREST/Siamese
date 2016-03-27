e1c1ae3456bb1f6f0441ce69cacb8444255c396b
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2016-03-27 21:19:14 +0100

    Added TF/IDF experiment

--- src/elasticsearch/Experiment.java
+++ src/elasticsearch/Experiment.java
@@ -1,22 +1,64 @@
 package elasticsearch;
 
 public class Experiment {
+	private static String[] normModes = {"x", "s", "w", "ws", "p", "ps", "pw", "pws", "k", "ks", "kw",
+			"kws", "kp", "kps", "kpw", "kpws", "j", "js", "jw", "jws", "jp",
+			"jps", "jpw", "jpws", "jk", "jks", "jkw", "jkws", "jkp", "jkps", "jkpw",
+			"jkpws", "d", "ds", "dw", "dws", "dp", "dps", "dpw", "dpws", "dk",
+			"dks", "dkw", "dkws", "dkp", "dkps", "dkpw", "dkpws", "dj", "djs", "djw",
+			"djws", "djp", "djps", "djpw", "djpws", "djk", "djks", "djkw", "djkws", "djkp",
+			"djkps", "djkpw", "djkpws"};
+	private static int[] ngramSizes = { 1, 2, 3, 4 };
+	
 	public static void main(String[] args) {
-		dfrExp();
+		// dfrExp();
+		tfidfExp();
+	}
+	
+	public static void tfidfExp() {
+		String[] discountOverlap = { "no", "true", "false" };
+		IndexChecker checker = new IndexChecker();
+				for (String discO : discountOverlap) {
+					String indexSettings = "";
+					if (!discO.equals("no")) 
+						indexSettings = "{"+
+										"\"settings\": {" +
+											"\"similarity\": {" +
+												"\"tfidf_similarity\": {" +
+													"\"type\": \"default\"," +
+													"\"discount_overlaps\": \"" + discO + "\"" +
+												"} } }, " +
+										"\"mappings\": { " +
+											"\"doc\": {" +
+												"\"properties\": { " +
+													"\"src\": { " +
+														"\"type\": \"string\"," +
+														"\"similarity\": \"tfidf_similarity\"" +
+													"} } } }, " +
+										"\"index\": { " +
+											"\"analysis\": { " +
+												"\"analyzer\": { " +
+													"\"default\": { " +
+														"\"type\": \"whitespace\"" +
+													"} } } } }";
+					else {
+						indexSettings = "{ \"settings\":"
+								+ "{ \"mappings\": {  \"doc\":{  \"properties\": "
+								+ "{  \"src\": { \"type\": \"string\", \"similarity\": \"default\" } } } }"
+								+ ", \"index\" : { \"analysis\" : { \"analyzer\" : "
+								+ "{ \"default\" : { \"type\" : \"whitespace\" } } } } } }";
+					}
+					// System.out.println(indexSettings);
+					checker.runExperiment("localhost", "tfidf_" + discO, "doc",
+							"/Users/Chaiyong/Documents/es_exp/methods", normModes, ngramSizes, true, true,
+							"/Users/Chaiyong/Documents/es_exp", false, indexSettings);
+				}
 	}
 	
 	public static void dfrExp() {
 		String[] basicModelArr = { "be", "d", "g", "if", "in", "ine", "p" };
 		String[] afterEffectArr = { "no", "b", "l" };
 		String[] dfrNormalizationArr = { "no", "h1", "h2", "h3", "z" };
-		String[] normModes = {"x", "s", "w", "ws", "p", "ps", "pw", "pws", "k", "ks", "kw",
-				"kws", "kp", "kps", "kpw", "kpws", "j", "js", "jw", "jws", "jp",
-				"jps", "jpw", "jpws", "jk", "jks", "jkw", "jkws", "jkp", "jkps", "jkpw",
-				"jkpws", "d", "ds", "dw", "dws", "dp", "dps", "dpw", "dpws", "dk",
-				"dks", "dkw", "dkws", "dkp", "dkps", "dkpw", "dkpws", "dj", "djs", "djw",
-				"djws", "djp", "djps", "djpw", "djpws", "djk", "djks", "djkw", "djkws", "djkp",
-				"djkps", "djkpw", "djkpws"};
-		int[] ngramSizes = { 1, 2, 3, 4 };
 		
 		IndexChecker checker = new IndexChecker();
 		
