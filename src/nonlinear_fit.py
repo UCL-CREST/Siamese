7b586bcfdd1886a424343e3a222343d727073508
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2018-02-06 17:33:49 +0000

    Updated the curve fitting script.

--- src/nonlinear_fit.py
+++ src/nonlinear_fit.py
@@ -1,31 +1,8 @@
 #!/usr/bin/env python
 from lmfit import minimize, Minimizer, Parameters, Parameter, report_fit
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-
-# create data to be fitted
-print('processing CSVs ...')
-file1='../freq_df_src.csv'
-df_src = pd.read_csv(file1, sep=',', header=0)
-df_src_sorted = df_src.sort_values(['freq'], ascending=False)
-df_src_sorted = df_src_sorted.reset_index(drop=True)
-df_src_sorted.index += 1
-df_src_sorted = df_src_sorted.reset_index()
-x = df_src_sorted.as_matrix(columns=['index'])
-data = df_src_sorted.as_matrix(columns=['freq'])
-# print(x)
-# print(y)
-# exit()
-
-# # define objective function: returns the array to be minimized
-# def fcn2min(params, x, data):
-#     """ model decaying sine wave, subtract data"""
-#     amp = params['amp']
-#     shift = params['shift']
-#     omega = params['omega']
-#     decay = params['decay']
-#     model = amp * np.sin(x * omega + shift) * np.exp(-x*x*decay)
-#     return model - data
 
 
 def zipf2min(params, x, data):
@@ -34,30 +11,104 @@ def zipf2min(params, x, data):
     b = params['b']
     a = params['a']
     model = c/(x + b)**a
+    # print(c, b, a, model)
     return model - data
 
 
-# create a set of Parameters
-params = Parameters()
-params.add('c',   value=2 * 10**5,  min=10**3, max=10**6, brute_step=100)
-params.add('b', value=1, min=1, max=3, brute_step=0.1)
-params.add('a', value=1, min=1., max=1.5, brute_step=0.01)
+def original():
+    # create data to be fitted
+    print('processing CSVs ...')
+    file1='../freq_df_toksrc_qualitas.csv'
+    df_src = pd.read_csv(file1, sep=',', header=0)
+    df_src_sorted = df_src.sort_values(['freq'], ascending=False)
+    df_src_sorted = df_src_sorted.reset_index(drop=True)
+    df_src_sorted.index += 1
+    df_src_sorted = df_src_sorted.reset_index()
+    # x = df_src_sorted.as_matrix(columns=['index'])[:100000, :1][:, 0]
+    # data = df_src_sorted.as_matrix(columns=['freq'])[:100000, :1][:, 0]
+    x = df_src_sorted.as_matrix(columns=['index'])[:, 0]
+    data = df_src_sorted.as_matrix(columns=['freq'])[:, 0]
+    # print(x)
+    # print(data)
+    # exit()
+    # create a set of Parameters
+    params = Parameters()
+    # params.add('c', value=10**6, min=10**5)
+    # params.add('b', value=2, min=0, max=5)
+    # params.add('a', value=1, min=0, max=3)
 
-# do fit, here with leastsq model
-minner = Minimizer(zipf2min, params, fcn_args=(x, data))
-result = minner.minimize()
+    params.add('c', value=3*(10**6), vary=False)
+    params.add('b', value=5, vary=False)
+    params.add('a', value=1.07, vary=False)
 
-# calculate final result
-final = data + result.residual
+    print('fitting the model ...')
+    # do fit, here with leastsq model
+    minner = Minimizer(zipf2min, params, fcn_args=(x, data))
+    result = minner.minimize()
+    # calculate final result
+    final = data + result.residual
+    # write error report
+    report_fit(result)
+    print('r^2 = ', 1 - result.residual.var() / np.var(data))
 
-# write error report
-report_fit(result)
+    # # try to plot results
+    try:
+        plt.plot(x, data, 'k+')
+        # plt.ylim(0, 50)
+        plt.plot(x, final, 'r')
+        plt.xscale("log", nonposx='clip')
+        plt.yscale("log", nonposy='clip')
+        plt.show()
+    except:
+        pass
 
-# try to plot results
-try:
-    import pylab
-    pylab.plot(x, data, 'k+')
-    pylab.plot(x, final, 'r')
-    pylab.show()
-except:
-    pass
+
+def normalised():
+    # create data to be fitted
+    print('processing CSVs ...')
+    file1='../freq_df_src_qualitas.csv'
+    df_src = pd.read_csv(file1, sep=',', header=0)
+    df_src_sorted = df_src.sort_values(['freq'], ascending=False)
+    df_src_sorted = df_src_sorted.reset_index(drop=True)
+    df_src_sorted.index += 1
+    df_src_sorted = df_src_sorted.reset_index()
+    # x = df_src_sorted.as_matrix(columns=['index'])[:1000, :1][:, 0]
+    # data = df_src_sorted.as_matrix(columns=['freq'])[:1000, :1][:, 0]
+    x = df_src_sorted.as_matrix(columns=['index'])[:, 0]
+    data = df_src_sorted.as_matrix(columns=['freq'])[:, 0]
+    # print(np.isnan(x).any())
+    # print(np.isnan(data).any())
+    # exit()
+    # create a set of Parameters
+    params = Parameters()
+    params.add('c', value=10**5, min=0)
+    params.add('b', value=2, min=0)
+    params.add('a', value=1.2, min=0)
+
+    print('fitting the model ...')
+    # do fit, here with leastsq model
+    minner = Minimizer(zipf2min, params, fcn_args=(x, data))
+    result = minner.minimize()
+    # calculate final result
+    final = data + result.residual
+    # write error report
+    report_fit(result)
+    print('r^2 = ', 1 - result.residual.var() / np.var(data))
+
+    # # try to plot results
+    try:
+        plt.plot(x, data, 'k+')
+        plt.plot(x, final, 'r')
+        plt.xscale("log", nonposx='clip')
+        plt.yscale("log", nonposy='clip')
+        plt.show()
+    except:
+        pass
+
+
+def main():
+    original()
+    # normalised()
+
+
+main()
