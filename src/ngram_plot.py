924de675dffc0d4e894eb1dd5ebe3b3628dd687c
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2018-03-09 18:58:56 +0000

    Added an n-gram size analysis plot.

--- src/ngram_plot.py
+++ src/ngram_plot.py
@@ -1,22 +1,50 @@
 from matplotlib import pyplot as plt
-import numpy as np
-import matplotlib
+from numpy import arange
 
-# Index time plot
+size = list(range(1, 22))
+cloplagm = [0.6078, 0.6887, 0.7247, 0.7263, 0.7198, 0.7222, 0.7345, 0.7353, 0.7361, 0.7345,
+            0.7328, 0.7328, 0.7304, 0.7198, 0.7222, 0.7132, 0.7132, 0.7083, 0.6920, 0.6871, 0.6838]
+cloplagf = [0.6030, 0.7790, 0.7910, 0.7860, 0.7970, 0.8000, 0.8030, 0.8000, 0.7960, 0.8060,
+            0.8140, 0.8160, 0.8230, 0.8130, 0.8040, 0.8070, 0.7990, 0.7910, 0.7810, 0.7790, 0.7770]
+soco = [0.6520, 0.8640, 0.9043, 0.9290, 0.9333, 0.9464, 0.9507, 0.9551, 0.9551, 0.9638,
+        0.9681, 0.9681, 0.9724, 0.9768, 0.9768, 0.9768, 0.9768, 0.9812, 0.9812, 0.9812, 0.9768]
 
-methods = [22, 50, 178, 423, 1723, 6601, 28030, 111190, 442403, 1771183, 4870113]
-siamese = [4.13, 2.95, 4.62, 8.4, 11.94, 36.22, 172.90, 614.90, 2077.04, 9089, 23606]
-scc = [(0.58 + 2.03), (0.88 + 0.68), (2.68 + 0.98), (3.81 + 1.37), (9.18 + 2.15), (28.40 + 4.96),
-       (110.09 + 16.78), (432.52 + 60.96), (1694.23 + 219.8), (6786 + 870.08), (18606 + 2348.9)]
-# seconds
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(methods, siamese, c="b", marker="s", label="Siamese")
-ax.plot(methods, scc, c="r", marker="x", label="SourcererCC")
-plt.yscale('log', basey=10)
-plt.xscale('log', basex=10)
-plt.xlabel("No. of methods")
-plt.ylabel("Indexing time (s)")
-plt.ylim(ymax=100000)
-plt.legend(loc=2)
-plt.show()
+ax.plot(size, cloplagm, c="b", marker="s", label="Ragkhitwetsagul (Method)")
+ax.plot(size, cloplagf, c="r", marker="x", label="Ragkhitwetsagul (File)")
+ax.plot(size, soco, c="g", marker="o", label="SOCO (File)")
+# plt.yscale('log', basey=10)
+# plt.xscale('log', basex=10)
+plt.xticks(arange(22))
+plt.xlabel("n-gram size")
+plt.ylabel("ARP")
+plt.legend(loc=4)
+# plt.show()
+
+fig = ax.get_figure()
+fig.savefig('ngram_arp.pdf', bbox_inches='tight')
+
+size = list(range(1, 22))
+cloplagm = [0.6745, 0.7626, 0.7972, 0.8039, 0.8050, 0.8062, 0.8105, 0.8116, 0.8134, 0.8125,
+            0.8113, 0.8098, 0.8119, 0.8075, 0.8053, 0.8022, 0.7981, 0.7937, 0.7818, 0.7788, 0.7737]
+cloplagf = [0.7207, 0.8644, 0.8727, 0.8642, 0.8712, 0.8796, 0.8831, 0.8850, 0.8890, 0.8963,
+            0.9013, 0.9000, 0.8988, 0.8919, 0.8852, 0.8875, 0.8824, 0.8774, 0.8738, 0.8725, 0.8721]
+soco = [0.6992, 0.8946, 0.9327, 0.9456, 0.9517, 0.9590, 0.9620, 0.9656, 0.9701, 0.9740,
+        0.9776, 0.9791, 0.9822, 0.9835, 0.9851, 0.9851, 0.9855, 0.9874, 0.9874, 0.9873, 0.9858]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(size, cloplagm, c="b", marker="s", label="Ragkhitwetsagul (Method)")
+ax.plot(size, cloplagf, c="r", marker="x", label="Ragkhitwetsagul (File)")
+ax.plot(size, soco, c="g", marker="o", label="SOCO (File)")
+# plt.yscale('log', basey=10)
+# plt.xscale('log', basex=10)
+plt.xticks(arange(22))
+plt.xlabel("n-gram size")
+plt.ylabel("ARP")
+plt.legend(loc=4)
+# plt.show()
+
+fig = ax.get_figure()
+fig.savefig('ngram_map.pdf', bbox_inches='tight')
