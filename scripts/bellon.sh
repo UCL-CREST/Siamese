4e6087abb90ebffb263b1465ed04fa19c3e4f4da
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2018-03-21 11:09:11 +0100

    Added configuration files for T2 n-gram analysis.

--- scripts/bellon.sh
+++ scripts/bellon.sh
@@ -1,19 +1,26 @@
-origIndexConfig="config_bellon_index.properties"
-origSearchConfig="config_bellon_search.properties"
+#!/bin/bash
+type=2
+origIndexConfig="config_bellon_index_" + $type + ".properties"
+origSearchConfig="config_bellon_search_" + $type + ".properties"
 
-for i in `seq 1 20`
+for i in `seq 6 40`
 do
-    echo $i
+    clear
+    echo "n-gram size: "$i
     iconfig="itemp.properties"
-    sconfig="stemp.preperties"
+    sconfig="stemp.properties"
 
     # create the config according the gram size
     cat $origIndexConfig | sed -e s/ngramSize=1/ngramSize=$i/g > $iconfig
     cat $origSearchConfig | sed -e s/ngramSize=1/ngramSize=$i/g > $sconfig
 
-    # index and search
-    mvn exec:java -Dexec.mainClass=crest.siamese.main.Main -Dexec.args="-cf $iconfig"
-    mvn exec:java -Dexec.mainClass=crest.siamese.main.Main -Dexec.args="-cf $sconfig"
+    # index
+    mvn exec:java -Dexec.mainClass=crest.siamese.main.Main -Dexec.args="-cf $iconfig" 2> /dev/null
+    # wait for the index to be ready to search
+    echo "sleeping ..."
+    sleep 60s
+    # start searching
+    mvn exec:java -Dexec.mainClass=crest.siamese.main.Main -Dexec.args="-cf $sconfig" 2> /dev/null
 
     # rename the result to match with the n-gram size
     mv /Users/Chaiyong/Documents/phd/2017/Siamese/exp_results/bellon_no_qr_* /Users/Chaiyong/Documents/phd/2017/Siamese/exp_results/$i.csv
