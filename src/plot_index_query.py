93e3e64562554e9330f827790462f0b099ddb8b4
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2018-04-15 11:33:55 +0000

    Latest update to the plotting script.

--- src/plot_index_query.py
+++ src/plot_index_query.py
@@ -11,9 +11,7 @@ jplag_methods = [22, 50, 178, 423, 1723, 6601, 28030]
 simian_methods = [22, 50, 178, 423, 1723, 6601, 28030]
 deckard_methods = [22, 50, 178, 423, 1723, 6601, 28030, 111190]
 
-siamese = [4.13, 2.95, 4.62, 8.4, 11.94,
-           36.22, 172.90, 614.90, 2077.04, 9089,
-           23606]
+siamese = [4.8, 5.26, 9.54, 19.21, 23.16, 97.07, 576.78, 1999.95, 6195, 25699, 74304]
 scc = [(0.58 + 2.03), (0.88 + 0.68), (2.68 + 0.98), (3.81 + 1.37), (9.18 + 2.15),
        (28.40 + 4.96), (110.09 + 16.78), (432.52 + 60.96), (1694.23 + 219.8), (6786 + 870.08),
        (18606 + 2348.9)]
@@ -63,34 +61,34 @@ plt.yscale('log', basey=10)
 plt.xscale('log', basex=10)
 plt.xlabel("No. of methods")
 plt.ylabel("Indexing time (m)")
-plt.ylim(ymax=1000)
+# plt.ylim(ymax=1500)
 plt.legend(loc=2)
 # plt.show()
 # fig = ax.get_figure()
 plt.savefig('../index_m.pdf', bbox_inches='tight')
 
 
-# # Query time plot
-#
-# methods = [22, 50, 178, 423, 1723, 6601, 28030, 111190, 442403, 1771183, 4870113]
-# # siamese = [1.539, 1.5405, 1.5526, 1.5244, 1.5818, 1.6382, 1.71, 1.8626, 1.9711, 2.3409, 3.30]
-# siamese = [1.7092, 1.6788, 1.8617, 1.7484, 1.7917, 1.8277, 1.7641, 2.0179, 2.2878, 2.7482, 2.9614]
-# scc = [1.3006, 1.3773, 1.3937, 1.3412, 1.4536, 1.4913, 2.0718, 3.3621, 9.2613, 28.3169, 60.9813]
-# scc_m = [x + 0.2354 for x in scc] # add avg tokenisation time
-#
-# print(scc_m)
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.plot(methods, siamese, c="b", marker="s", label="Siamese")
-# ax.plot(methods, scc_m, c="r", marker="x", label="SourcererCC")
-# plt.yscale('log', basey=10)
-# plt.xscale('log', basex=10)
-# plt.ylim(ymax=100)
-# plt.xlabel("No. of methods in the index")
-# plt.ylabel("Average query response time (s)")
-# plt.legend(loc=2)
-# plt.show()
-#
-# fig = ax.get_figure()
-# fig.savefig('query.pdf', bbox_inches='tight')
+# Query time plot
+
+methods = [22, 50, 178, 423, 1723, 6601, 28030, 111190, 442403, 1771183, 4870113]
+# siamese = [1.539, 1.5405, 1.5526, 1.5244, 1.5818, 1.6382, 1.71, 1.8626, 1.9711, 2.3409, 3.30]
+siamese = [1.7297, 1.7503, 1.7412, 1.7682, 1.7966, 2.06, 2.5026, 2.6474, 5.3371, 5.5473, 9.2843]
+scc = [1.3006, 1.3773, 1.3937, 1.3412, 1.4536, 1.4913, 2.0718, 3.3621, 9.2613, 28.3169, 60.9813]
+scc_m = [x + 0.2354 for x in scc] # add avg tokenisation time
+
+print(scc_m)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(methods, siamese, c="b", marker="s", label="Siamese")
+ax.plot(methods, scc_m, c="r", marker="x", label="SourcererCC")
+plt.yscale('log', basey=10)
+plt.xscale('log', basex=10)
+plt.ylim(ymax=100)
+plt.xlabel("No. of methods in the index")
+plt.ylabel("Average query response time (s)")
+plt.legend(loc=2)
+plt.show()
+
+fig = ax.get_figure()
+fig.savefig('../query.pdf', bbox_inches='tight')
