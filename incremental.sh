69218cccd447c410c3036396e68929c16396d6f3
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2018-04-22 15:10:42 +0100

    Added configuration files and the updated script

--- incremental.sh
+++ incremental.sh
@@ -18,3 +18,21 @@ rm 0.10.0.zip
 cd $SHOME
 java -jar siamese-0.0.5-SNAPSHOT.jar -cf config_incremental_index.properties
 rm -rf $PROJHOME/unzipped/*
+cd $PROJHOME
+for i in `ls *.zip`; do 
+    echo $i
+	cp $i unzipped 
+	cd unzipped 
+	unzip $i  > /dev/null
+	ls -l 
+	rm $i 
+	# read
+	cd $SHOME
+    echo ">> START-UPDATE"
+	/usr/bin/time -v java -jar siamese-0.0.5-SNAPSHOT.jar -cf config_delete_incremental.properties
+	/usr/bin/time -v java -jar siamese-0.0.5-SNAPSHOT.jar -cf config_incremental_index.properties
+    echo ">> END-UPDATE"
+	cd $PROJHOME/unzipped
+	rm -rf elasticsearch*
+	cd ..
+done
