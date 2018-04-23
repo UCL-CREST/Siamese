4078d545c66a4217b5e55e8edd292d639668e025
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2018-04-23 03:17:36 +0100

    Updated the script

--- incremental.sh
+++ incremental.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+if [ "$#" -ne 2 ] || ! [ -d "$1" ]; then
     echo "=============================================================================="
     echo "A script to perform Siamese incremental update"
-    echo "Usage: $0 DIRECTORY" >&2
+    echo "Usage: $0 DIRECTORY STARTING_FILE" >&2
     echo "=============================================================================="
     exit 1
 fi
 
 PROJHOME=$1
+START=$2
 SHOME=/home/cragkhit/data/siamese_study/Siamese
 rm -rf $PROJHOME/unzipped/*
-cp $PROJHOME/0.10.0.zip $PROJHOME/unzipped
+cp $PROJHOME/$2 $PROJHOME/unzipped
 cd $PROJHOME/unzipped
-unzip 0.10.0.zip > /dev/null
-rm 0.10.0.zip
+unzip $2 > /dev/null
+rm $2
 
 cd $SHOME
 java -jar siamese-0.0.5-SNAPSHOT.jar -cf config_incremental_index.properties
