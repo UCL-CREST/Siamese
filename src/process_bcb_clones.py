60e460738e2708e47c969595b69722fa2a3ef550
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2018-08-14 15:45:52 +0700

    Updated a configuration file and the script to process BigCloneBench clone pairs.

--- src/process_bcb_clones.py
+++ src/process_bcb_clones.py
@@ -9,14 +9,16 @@ def gen_pairs(line):
 
 def format_query(query, prefix):
     parts = query.split('#')
-    return parts[0].replace(prefix, '') + ',' + parts[1] + '.java,' + parts[2] + ',' + parts[3].replace('.java_method', '')
+    return parts[0].replace(prefix, '').strip() + ',' \
+           + parts[1].strip() + '.java,' + parts[2].strip() + ',' \
+           + parts[3].replace('.java_method', '').strip()
 
 
 def format_clone(clone, prefix):
     parts = clone.split('#')
     filepart = parts[0].split('.java_')
     filename = filepart[0].replace(prefix, '').replace('/', ',') + '.java'
-    return filename + ',' + parts[1] + ',' + parts[2]
+    return filename.strip() + ',' + parts[1].strip() + ',' + parts[2].strip()
 
 
 file = open(sys.argv[1], 'r')
@@ -24,7 +26,6 @@ for line in file:
     parts = gen_pairs(line)
     iterparts = iter(parts)
     query = next(iterparts)
-    # print(format_query(query, '/Users/Chaiyong/Documents/phd/2017/Siamese/bcb_clones/'))
     for part in iterparts:
-        print(format_query(query, '/Users/Chaiyong/Documents/phd/2017/Siamese/bcb_clones/') + ',' +
-              format_clone(part, '/Users/Chaiyong/Downloads/bcb_dataset/'))
+        print(format_query(query, '/scratch0/NOT_BACKED_UP/crest/cragkhit/siamese/') + ',' +
+              format_clone(part, '/scratch0/NOT_BACKED_UP/crest/cragkhit/dataset/'))
