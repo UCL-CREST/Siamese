574c51f719da9cca897209bab45ced76885478ea
mrhmisu <md.rakib.hossain.19@ucl.ac.uk>
2020-07-06 14:02:12 +0100

    Fix JSTokenizer and Start writing Unit Test

--- src/main/java/crest/siamese/language/javascript/JSTokenizer.java
+++ src/main/java/crest/siamese/language/javascript/JSTokenizer.java
@@ -4,7 +4,6 @@ import crest.siamese.language.Normalizer;
 import crest.siamese.language.Tokenizer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.File;
 import java.io.Reader;
@@ -30,49 +29,50 @@ public class JSTokenizer implements Tokenizer {
     }
 
     @Override
-    public ArrayList<String> tokenize(String s) throws NotImplementedException {
+    public ArrayList<String> tokenize(String s) throws Exception {
         return null;
     }
 
     @Override
-    public ArrayList<String> tokenize(Reader reader) throws NotImplementedException {
+    public ArrayList<String> tokenize(Reader reader) throws Exception {
         return null;
     }
 
     @Override
-    public ArrayList<String> tokenizeLine(Reader reader) throws NotImplementedException {
+    public ArrayList<String> tokenizeLine(Reader reader) throws Exception {
         return null;
     }
 
     @Override
-    public ArrayList<String> tokenize(File f) throws NotImplementedException {
+    public ArrayList<String> tokenize(File f) throws Exception {
         return null;
     }
 
     @Override
-    public ArrayList<String> getTokensFromFile(String file) throws NotImplementedException {
+    public ArrayList<String> getTokensFromFile(String file) throws Exception {
         return null;
     }
 
     /**
-     * Uses the ANTLR4 JavaScriptLexer to obtain the list of tokens from the extracted method. Normalises the tokens
+     * Uses the ANTLR4 Python3Lexer to obtain the list of tokens from the extracted method. Normalises the tokens
      * according to the configuration of the Normalizer (composite object of this class).
      *
      * @param input input
      * @return ArrayList of String
      */
-
-
     @Override
-    public ArrayList<String> getTokensFromString(String input) throws Exception {
+    public ArrayList<String> getTokensFromString(String input) {
         JavaScriptLexer lexer = new JavaScriptLexer(CharStreams.fromString(input));
         List<? extends Token> tokens = lexer.getAllTokens();
-        return tokens.stream()
-                .map(token -> normalizer.normalizeAToken(
-                        token.getText(),
-                        JavaScriptLexer.VOCABULARY.getSymbolicName(token.getType())))
+        List<Token> normalizedTokens = new ArrayList<>();
+        tokens.forEach(token -> {
+            if (!JavaScriptParser.VOCABULARY.getSymbolicName(token.getType()).equals("WhiteSpaces")) {
+                normalizedTokens.add(token);
+            }
+        });
+        return normalizedTokens.stream()
+                .map(token -> normalizer.normalizeAToken(token.getText(),
+                        JavaScriptParser.VOCABULARY.getSymbolicName(token.getType()).toUpperCase()))
                 .collect(Collectors.toCollection(ArrayList::new));
-
-
     }
 }
--- src/test/java/crest/siamese/language/javascript/JSTokenizerTest.java
+++ src/test/java/crest/siamese/language/javascript/JSTokenizerTest.java
@@ -1,4 +1,32 @@
 package crest.siamese.language.javascript;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class JSTokenizerTest {
+
+
+    @Test
+    public void getTokensFromStringTest() {
+        // given
+        JSNormalizerMode jsNormalizerMode = new JSNormalizerMode();
+        JSNormalizer jsNormalizer = new JSNormalizer();
+        jsNormalizer.configure(jsNormalizerMode);
+        JSTokenizer jsTokenizer = new JSTokenizer();
+        jsTokenizer.configure(jsNormalizer);
+        // when
+        String input = "function (a,b){ return a+b;} ";
+        ArrayList<String> actual = jsTokenizer.getTokensFromString(input);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList(
+                "function", "(", "a", ",", "b", ")", "{", "return", "a", "+", "b", ";", "}"
+        ));
+        // then
+        assertThat(actual, is(expected));
+
+    }
 }
